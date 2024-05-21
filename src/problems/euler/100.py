1b8ef484929d48c255a108ead99ce88371bfe32c
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-05-21 07:42:28 -0400

    Solve Euler 2

--- src/problems/euler/100.py
+++ src/problems/euler/100.py
@@ -6,3 +6,20 @@ def solve_one():
         lambda x, y: x+y,
         filter(lambda x: x % 3 == 0 or x % 5 == 0, range(1000))
     )
+
+
+def solve_two():
+    x = 1
+    y = 2
+    sum = 2
+
+    while (y < 4e6):
+        tmp = y
+        y = x+y
+        x = tmp
+        print(y)
+
+        if y % 2 == 0:
+            sum += y
+
+    return sum
