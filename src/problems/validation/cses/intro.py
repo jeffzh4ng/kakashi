c957b1028556f21e763dca7009e3ac6242ea8ee2
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-05-23 15:58:36 -0400

    Solve CSES 4

--- src/problems/validation/cses/intro.py
+++ src/problems/validation/cses/intro.py
@@ -37,3 +37,38 @@ def solve_three():
 
     max_count = max(max_count, cur_count)
     return max_count
+
+
+def solve_four():
+    n = int(input())
+    inp = list(map(int, input().split(" ")))
+    outp = 0
+
+    for i in range(len(inp)-1):
+        if inp[i] > inp[i+1]:
+            outp += inp[i] - inp[i+1]
+            inp[i+1] = inp[i]
+
+    return outp
+
+
+print(solve_four())
+
+
+# idea 1:
+# - walk through the list?
+#   - i: how would i know how to update i??
+#   - can i somehow update just based off a neighbor update?
+#   - lowering i to i+1 heuristic does not work
+#   counter:
+#     6 10 4 10 2 8 9 2 7 7
+#     6 4 4 10 2 8 9 2 7 7 <---- fail. 10 is lowered down to 4 which breaks monotonicity
+
+# can we reverse the heuristic to increasing in order to maintain the monotonic invariant?
+
+
+# idea 2:
+# - is there an analytic formula here?
+# - what if i sum the numbers
+# - that's going to lose all the information though
+# - bad idea
