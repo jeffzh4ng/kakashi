d1d0f250ae37ec501aa7b80eb4b6768a44d33b93
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-05-23 11:34:10 -0400

    Solve CSES 2

--- src/problems/validation/cses/intro.py
+++ src/problems/validation/cses/intro.py
@@ -1,7 +1,8 @@
 from sys import stdin
+from functools import reduce
 
 
-def solve_weird_algorithm():
+def solve_one():
     n = int(input())
     output = [n]
 
@@ -12,3 +13,11 @@ def solve_weird_algorithm():
             output.append(output[-1]*3+1)
 
     return " ".join(map(str, output))
+
+
+def solve_two():
+    n = int(input())
+    inp = map(int, input().split(" "))
+    output = reduce(lambda x, y: x - y, inp, (n*(n+1))//2)
+
+    return output
